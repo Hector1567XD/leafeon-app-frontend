d230f506ed2563bb540db02b4b8eee27ce5a87c8
Feredev <heferrer.19@est.ucab.edu.ve>
2023-07-03 14:29:27 -0400

    Convert sample page on typescript

---
+++ src/ui-component/cards/MainCard.tsx
@@ -0,0 +1,84 @@
+import { forwardRef, HTMLAttributes } from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
+
+// constant
+const headerSX = {
+  '& .MuiCardHeader-action': { mr: 0 }
+};
+
+// ==============================|| CUSTOM MAIN CARD ||============================== //
+
+const MainCard = forwardRef<HTMLDivElement, MainCardProps>(
+  (
+    {
+      border = true,
+      boxShadow,
+      children,
+      content = true,
+      contentClass = '',
+      contentSX = {},
+      darkTitle,
+      secondary,
+      shadow,
+      sx = {},
+      title,
+      ...others
+    },
+    ref
+  ) => {
+    const theme = useTheme<any>();
+
+    return (
+      <Card
+        ref={ref}
+        {...others}
+        sx={{
+          border: border ? '1px solid' : 'none',
+          borderColor: theme.palette.primary[200] + 25,
+          ':hover': {
+            boxShadow: boxShadow ? shadow || '0 2px 14px 0 rgb(32 40 45 / 8%)' : 'inherit'
+          },
+          ...sx
+        }}
+      >
+        {/* card header and action */}
+        {title && 
+          <CardHeader
+            sx={headerSX as any}
+            title={(darkTitle ? <Typography variant="h3">{title as any}</Typography> : title) as any}
+            action={secondary as any} 
+          />
+        }
+
+        {/* content & header divider */}
+        {title && <Divider />}
+
+        {/* card content */}
+        {content && (
+          <CardContent sx={contentSX} className={contentClass}>
+            {children}
+          </CardContent>
+        )}
+        {!content && children}
+      </Card>
+    );
+  }
+);
+
+interface MainCardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'title' | 'content'> {
+  border?: boolean;
+  boxShadow?: boolean;
+  content?: boolean;
+  contentClass?: string;
+  contentSX?: object;
+  sx?: Record<string, any>;
+  darkTitle?: boolean;
+  secondary?: React.ReactNode | string | object;
+  shadow?: string;
+  title?: React.ReactNode | string | object;
+}
+
+export default MainCard;
