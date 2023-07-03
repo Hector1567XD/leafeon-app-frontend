f9c0bed11c74cfd5bfa332bc16a79fb8e8a68663
Feredev <heferrer.19@est.ucab.edu.ve>
2023-07-03 14:41:27 -0400

    Deleted unused components

--- src/views/sample-page/index.tsx
+++ src/views/sample-page/index.tsx
@@ -8,10 +8,9 @@ const SamplePage = () => {
   return (
     <MainCard title="Sample Card">
       <Typography variant="body2">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-        in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-        descent molls anim id est labours.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quis in qui nemo.
+        Nesciunt iure quidem ullam iste nulla, alias numquam aliquid
+        architecto praesentium molestias officia incidunt enim, ipsa repellat.
       </Typography>
       <Table />
     </MainCard>
