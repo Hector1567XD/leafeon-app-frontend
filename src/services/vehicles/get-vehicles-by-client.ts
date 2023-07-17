ceb1273dc04f1723d0ab9e5589f8475220aeb4d7
Feredev <aurimart.liliana@gmail.com>
2023-07-16 22:14:59 -0400

    vehicles

---
+++ src/services/vehicles/get-vehicles-by-client.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+// Own
+import { API_BASE_URL } from 'config/constants';
+import { Vehicle } from 'core/vehicles/types';
+import BackendError from 'exceptions/backend-error';
+import store from 'store';
+
+const URL = `${API_BASE_URL}/vehicles/client`;
+
+export default async function getVehicleByClient(clietnDni: string | null): Promise<Vehicle[]> {
+  try {
+    const response = await axios.get<Vehicle[]>(
+        `${URL}/${clietnDni}`, {
+        headers: {
+          Authorization: `Bearer ${store.getState().auth.token}`,
+        }
+      }
+    );
+    return response.data;
+  } catch (error: unknown) {
+    console.log(error);
+    throw new BackendError(error);
+  }
+}
